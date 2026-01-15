06303fd48a9cf5568e5c89c27c0791efe593e531
lalapisay <b2029laepauig@pshs.edu.ph>
2026-01-15 10:36:20 +0800

    commit

---
+++ public/2ndgraded.js
@@ -0,0 +1,32 @@
+let acctString = localStorage.getItem("accounts")
+if (!acctString) { accountList = {} } 
+else accountList = JSON.parse(acctString) 
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    if (confirm("Save your info?")){
+
+        const data = new FormData(form);
+        const obj = Object.fromEntries(data.entries());
+
+        accountList[obj.uname] = {};
+        for (let key in obj){
+            if (key != "uname") {
+                accountList[obj.uname][key] = obj[key];
+            }
+        }
+
+        console.log(accountList)
+        acctString = JSON.stringify(accountList)
+        localStorage.setItem("accounts", acctString)
+        form.submit();
+    }
+});
+
+form.addEventListener("reset", function(e) {
+    if(!confirm("Clear your data?")){
+        e.preventDefault();
+    }
+});
